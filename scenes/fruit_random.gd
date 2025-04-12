0449497a10936b359b59f7a71fa849b77c6123a1
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2025-04-11 22:17:51 -0300

    feat: add chicken enemy and enhance fruit randomization

--- scenes/fruit_random.gd
+++ scenes/fruit_random.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+class_name FruitRandom
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
-# Called when the node enters the scene tree for the first time.
+signal fruit_eaten
+
+@export var list_fruits: Array[Texture2D]
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var n = randi_range(0, list_fruits.size()-1)
+	sprite_2d.texture = list_fruits[n]
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	animation_player.play("apple")
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	fruit_eaten.emit()
+	queue_free()
--- scenes/level_0.gd
+++ scenes/level_0.gd
@@ -4,13 +4,15 @@ extends Node2D
 @onready var itens: Node2D = $Itens
 
 var num_fruit:int = 0
+var initial_position = Vector2(60, 179)
 
 func _ready() -> void:
 	for item in itens.get_children():
-		if item is Fruit:
+		if item is Fruit or FruitRandom:
 			item.fruit_eaten.connect(_on_fruit_eaten)
 
 
 func _on_fruit_eaten():
 	num_fruit += 1
+	player.global_position = initial_position
 	print("Já comeu ", num_fruit)
